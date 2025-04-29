af41d944a9d1a6eea3a2b78b2a0288dbaf2ecf94
Plvltra <331079072@qq.com>
2025-04-29 15:09:32 +0800

    check2: the TCP receiver

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,18 +1,18 @@
 #include "wrapping_integers.hh"
-#include "debug.hh"
+#include <cstdint>
 
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  return Wrap32 { zero_point + n };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  return {};
+  uint64_t rounder = (uint64_t)1 << 32;
+  uint32_t diff = raw_value_ - Wrap32::wrap( checkpoint, zero_point ).raw_value_;
+  if ( diff <= ( rounder >> 1 ) || checkpoint + diff < rounder )
+    return checkpoint + diff;
+  return checkpoint + diff - rounder;
 }
